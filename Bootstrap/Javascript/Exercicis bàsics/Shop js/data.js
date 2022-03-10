1bce91aba726ccfed31bd5ef4d617b20fb32a151
icardus <ignasi.cardus@gmail.com>
2022-03-10 13:57:35 +0100

    canvis

---
+++ Bootstrap/Javascript/Exercicis bàsics/Shop js/data.js
@@ -0,0 +1,16 @@
+var products = [
+    {
+    id: 1,
+    name: 'cooking oil',
+    price: 10.5,
+    type: 'grocery'
+    },
+    {
+    id: 2,
+    name: 'Pasta',
+    price: 6.25,
+    type: 'grocery'
+    }
+    ]
+
+    
