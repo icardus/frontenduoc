e1d01ca5d9aaeb4756f03f8a9eb08052b1603678
icardus <ignasi.cardus@gmail.com>
2022-03-16 19:42:57 +0100

    dom

---
+++ DOM/js/index.js
@@ -0,0 +1,28 @@
+/*Exercici 1
+Exercici 1: Accedint al document
+Mostra per consola els següent element DOM:
+• document
+• body
+• id del formulari
+• el links
+• el className d’algun dels links
+• totes les imatges*/
+
+
+
+/*Exercici 2: Accedim a les classes
+Mostra per consola
+• el header ( class = header )
+• el contenido-hero ( class = contenido-hero )
+• els contenidors ( class = contenedor)*/ 
+
+let header = document.getElementsByClassName("header");
+console.log(header);
+
+let contenido_hero = document.getElementsByClassName("contenido-hero");
+console.log(contenido_hero);
+
+let contenidors = document.getElementsByClassName("contenedor");
+console.log(contenidor);
+
+
