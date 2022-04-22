26ad7305461303b4f5fa0bad540895b240d661dc
icardus <ignasi.cardus@gmail.com>
2022-04-22 12:58:28 +0200

    api vue

--- vuecli/vue-first-app/nouapp/src/App.vue
+++ vuecli/vue-first-app/nouapp/src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
       <router-link to="/contact">Contact</router-link>
     </nav>
+    <img id="tetera" src="src\assets\tetera.jpg" alt="">
     <router-view/>
   </div>
 </template>
--- vuecli/vue-first-app/nouapp/src/views/AboutView.vue
+++ vuecli/vue-first-app/nouapp/src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Prova de APIs</h1>
+    <h2>Acudit del dia</h2>
+    {{acudit}}
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      post: null,
+      acudit: "",
+    };
+  },
+  mounted() {
+    console.log("entra");
+    axios
+      .get("https://api.chucknorris.io/jokes/random")
+      .then((response) => (this.acudit = response.data.value));
+  },
+};
+</script>
+
+
